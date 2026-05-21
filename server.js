baa2d9bb3edc37ec84b74eba55a112e307c753fc
Moiz Khatri <khatrimoiz86@gmail.com>
2026-05-21 11:04:16 +0500

    vercel file

--- server.js
+++ server.js
@@ -51,7 +51,11 @@ app.use(errorHandler);
 
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`);
+    });
+}
+
+export default app;
